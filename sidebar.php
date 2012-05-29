48b8f3ed6fc767ee0cb5a0af33a1928f1371137c
vitrum.zhu <vitrum.zhu@hotmail.com>
2012-05-29 18:23:27 +0800

    move to themes

---
+++ sidebar.php
@@ -0,0 +1,6 @@
+<div class="well">
+  <?php dynamic_sidebar('Right Sidebar1'); ?>
+</div>
+<div class="well">
+  <?php dynamic_sidebar('Right Sidebar2'); ?>
+</div>
---
+++ sidebar2.php
@@ -0,0 +1,9 @@
+<div class="well">
+  <?php dynamic_sidebar('Header Top Sidebar'); ?>
+</div>
+<div class="well">
+  <?php dynamic_sidebar('Header banner Sidebar'); ?>
+</div>
+<div class="well">
+  <?php dynamic_sidebar('Header logo Sidebar'); ?>
+</div>
