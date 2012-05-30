0f4e7b3e14b7da418b599384b58a1ab4f92779a8
vitrum.zhu <vitrum.cn@gmail.com>
2012-05-31 07:26:48 +0800

    update test

--- sidebar2.php
+++ sidebar2.php
@@ -1,9 +1,9 @@
-<div class="well">
+<div class="span12">
   <?php dynamic_sidebar('Header Top Sidebar'); ?>
 </div>
-<div class="well">
+<div class="span12">
   <?php dynamic_sidebar('Header banner Sidebar'); ?>
 </div>
-<div class="well">
+<div class="span12">
   <?php dynamic_sidebar('Header logo Sidebar'); ?>
 </div>
