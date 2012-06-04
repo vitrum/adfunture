ca2eaf92ffc9f371561fd69b82c7bdba2110b7e0
vitrum.zhu <vitrum.zhu@hotmail.com>
2012-06-04 17:19:13 +0800

    css

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
 <div class="row">
-	<div id="silderbar-left" class="span2 gzwall">
+	<div id="silderbar-left" class="span2">
 	  <?php dynamic_sidebar('Right Sidebar1'); ?>
 	</div>
-	<div id="silderbar-right" class="span2 gzwall ">
+	<div id="silderbar-right" class="span2 ">
 	  <?php dynamic_sidebar('Right Sidebar2'); ?>
 	</div>
 </div>
--- sidebar2.php
+++ sidebar2.php
@@ -5,9 +5,11 @@
 	  <?php dynamic_sidebar('Header Top Sidebar'); ?>
 	</div>
 </header>
+<div class="gzline"></div>
 <div class=" topsliderbanner gzwall">
   <?php dynamic_sidebar('Header banner Sidebar'); ?>
 </div>
+<div class="gzline"></div>
 <div class=" toplogobanner gzwall">
   <?php dynamic_sidebar('Header logo Sidebar'); ?>
 </div>
